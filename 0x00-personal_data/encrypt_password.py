2b8bb00154dc33820734b9fe3a8738f60e554a97
chriss1525 <okothchris15@gmail.com>
2023-11-02 14:25:27 +0300

    function that uses the bycript package to hash passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+""" Hashing Passwords """
+
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
+    """ use bcrypt to hash a password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
