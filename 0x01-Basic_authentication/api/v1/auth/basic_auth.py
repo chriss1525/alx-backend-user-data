ed0edb64c66fc8b15f4505843d3b94d83137d8ed
chriss1525 <okothchris15@gmail.com>
2023-11-08 00:49:37 +0300

    impliment basic authentication for the api

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import *
+from functools import wraps
+
+class BasicAuth(Auth):
+    """implement basic auth for flask
+    """
+    pass
+
