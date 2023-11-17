f60ec97a3d81a286de549ae4b82e480a9ae67d80
chriss1525 <okothchris15@gmail.com>
2023-11-17 11:29:29 +0300

    add user to db

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,7 +4,6 @@
 """ user madel for a database"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
