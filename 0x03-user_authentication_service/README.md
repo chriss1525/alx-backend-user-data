d05a0e939748322e47b4f1200b830f18860a5386
chriss1525 <okothchris15@gmail.com>
2023-11-17 10:53:54 +0300

    create user table in sqlalchemy orm

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,3 @@
+# User Authentication Serivice
+
+Learning how to properly secure user data and authenticate a user when they log in
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" user madel for a database"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ User class for a database """
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
