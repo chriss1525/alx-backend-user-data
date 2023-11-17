10176726d24b924bfd8df86d57e3b1df805e2443
chriss1525 <okothchris15@gmail.com>
2023-11-17 11:41:47 +0300

    find user using certain parameters

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,3 +39,12 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Returns the first row found in the users table as filtered by the
+        method’s input arguments
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        return user
