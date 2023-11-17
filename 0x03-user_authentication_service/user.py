9ddb2bb6ce7039658445aea855272bb0d7e08b6d
chriss1525 <okothchris15@gmail.com>
2023-11-17 10:58:38 +0300

    create user table in sqlalchemy orm

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -20,3 +20,8 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=False)
     reset_token = Column(String(250), nullable=False)
+
+    def __repr__(self):
+        """ return a string rep of the user object """
+
+        return f"User: id={self.id}"
